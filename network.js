3b33c10f7352a209d067ca6029954a8ad810ed14
Evgeniy Vasilyev <ju5tc4t@gmail.com>
2018-10-04 00:10:31 +0300

    Cleanup codestyle, warnings, line separator. Again.

--- network.js
+++ network.js
@@ -1,37 +1,37 @@
-class Node{
-	//number, x, y;
-	constructor(_x, _y){
-		this.x = _x;
-		this.y = _y;
-	}
+class Node {
+    //number, x, y;
+    constructor(_x, _y) {
+        this.x = _x;
+        this.y = _y;
+    }
 }
 
-class Edge{
-	//Node startNode, endNode
-	//Type: Empty, Resistor, Condensator, Coil, Switch, Lamp
-	//Resistor: resistance
-	//Condensator: capacity
-	//Coil: inductance
-	//Switch: open
-	constructor(sNode, eNode, t, param = 0){
-		this.startPoint = sNode;
-		this.endPoint = eNode;
-		this.type = t;
-		switch(t){
-			case 0: //Empty
-				break;
-			case 1: //Resistor
-			case 5: //Lamp
-				this.resistance = param;
-				break;
-			case 2: //Condensator
-				this.capacity = param;
-				break;
-			case 3: //Coil
-				this.inductance = param;
-				break;
-			case 4: //Switch
-				this.state = param; 
-		}
-	}
+class Edge {
+    //Node startNode, endNode
+    //Type: Empty, Resistor, Condensator, Coil, Switch, Lamp
+    //Resistor: resistance
+    //Condensator: capacity
+    //Coil: inductance
+    //Switch: open
+    constructor(sNode, eNode, t, param = 0) {
+        this.startPoint = sNode;
+        this.endPoint = eNode;
+        this.type = t;
+        switch (t) {
+            case 0: //Empty
+                break;
+            case 1: //Resistor
+            case 5: //Lamp
+                this.resistance = param;
+                break;
+            case 2: //Condensator
+                this.capacity = param;
+                break;
+            case 3: //Coil
+                this.inductance = param;
+                break;
+            case 4: //Switch
+                this.state = param;
+        }
+    }
 }
