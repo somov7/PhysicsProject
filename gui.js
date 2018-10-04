9455394a606b177ed4dcbd2fc96b3a410fb212f8
somov7 <39019505+somov7@users.noreply.github.com>
2018-10-04 21:16:31 +0300

    Add files via upload

---
+++ gui.js
@@ -0,0 +1,18 @@
+function updateHTML(){
+	let inCur = document.getElementById('inputCurrent');
+	let inOption = inCur.options[inCur.selectedIndex].value;
+	if(inOption == "direct")
+		document.getElementById('inputFrequencyWrap').setAttribute("hidden", "hidden");
+	else
+		document.getElementById('inputFrequencyWrap').removeAttribute("hidden");
+}
+
+let canvasHover = false;
+
+canv.onmouseover = function(e) {
+	canvasHover = true;
+};
+
+canv.onmouseout = function(e) {
+	canvasHover = false;
+};
