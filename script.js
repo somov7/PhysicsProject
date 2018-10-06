adc952cce290f91ee1219119b6a9ccd2a5d508b4
Evgeniy Vasilyev <ju5tc4t@gmail.com>
2018-10-06 13:56:30 +0300

    Cleanup codestyle, fix warnings.

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 let network = new Network();
 
-function cycle(){
-	ctx.save();
-	ctx.clearRect(0, 0, canv.width, canv.height);
-	ctx.scale(1.5, 1.5);
-	initialDraw();
-	drawNetwork(network);
-	updateHTML();
-	ctx.restore();
-	window.requestAnimationFrame(cycle);	
+function cycle() {
+    ctx.save();
+    ctx.clearRect(0, 0, canv.width, canv.height);
+    ctx.scale(1.5, 1.5);
+    initialDraw();
+    drawNetwork(network);
+    updateHTML();
+    ctx.restore();
+    window.requestAnimationFrame(cycle);
 }
 
-function initSampleNetwork(){
-	
-	network.addNode(8, 1);
-	network.addNode(5, 1);
-	network.addNode(5, 6);
-	network.addNode(10, 6);
-	network.addNode(14, 6);
-	network.addNode(14, 1);
-	network.addNode(11, 1);
-	
-	network.addEdge(0, 1, 0);
-	network.addEdge(1, 2, 1);
-	network.addEdge(2, 3, 2);
-	network.addEdge(3, 4, 3);
-	network.addEdge(4, 5, 4);
-	network.addEdge(5, 6, 5);
+function initSampleNetwork() {
+
+    network.addNode(8, 1);
+    network.addNode(5, 1);
+    network.addNode(5, 6);
+    network.addNode(10, 6);
+    network.addNode(14, 6);
+    network.addNode(14, 1);
+    network.addNode(11, 1);
+
+    network.addEdge(0, 1, 0);
+    network.addEdge(1, 2, 1);
+    network.addEdge(2, 3, 2);
+    network.addEdge(3, 4, 3);
+    network.addEdge(4, 5, 4);
+    network.addEdge(5, 6, 5);
 }
 
 initSampleNetwork();
